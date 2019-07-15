eac15991d2dbfa68b88ab36bc289cc40d13db42c
darkthecross <darkthecross@gmail.com>
2019-07-14 20:21:56 -0700

    Replace projects with dummy posts.

--- assets/default.js
+++ assets/default.js
@@ -1,3 +1,7 @@
+/*jshint esversion: 6 */
+
+// Navbar animation
+
 function showNav() {
     document.getElementById('nav-bar').classList.remove('nav-hidden');
     document.getElementById('nav-bar').classList.add('nav-show');
@@ -10,7 +14,7 @@ function hideNav() {
 
 var lastScrollPos = window.pageYOffset;
 
-document.onscroll = function () {
+document.addEventListener('scroll', function () {
     var currScrollPos = window.pageYOffset;
     if (currScrollPos > lastScrollPos && currScrollPos > 200){
         hideNav();
@@ -18,4 +22,19 @@ document.onscroll = function () {
         showNav();
      }
      lastScrollPos = currScrollPos;
-};
+});
+
+// Project cards action
+
+var projectCards = document.getElementsByClassName('project-card');
+for(var el of projectCards)
+{
+    el.addEventListener('mouseenter', function(e) {
+        this.classList.remove('shadow-sm');
+        this.classList.add('shadow-lg');
+    });
+    el.addEventListener('mouseleave', function(e) {
+        this.classList.remove('shadow-lg');
+        this.classList.add('shadow-sm');
+    });
+}
